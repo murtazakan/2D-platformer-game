5939b811b511e77fe63c0b5b8c68dd23ac2a99b3
murkeh <kanorwalamurtaza217@gmail.com>
2020-09-19 17:25:34 +0530

    implemented 3 scenes and 6 buttons to work as assignment 1 intended

---
+++ Assets/Scripts/buttons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour
+{
+    public Button b1, b2, b3;
+
+
+    public void LoadSceneOnClick(int sceneNo)
+    {
+        SceneManager.LoadScene(sceneNo);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
