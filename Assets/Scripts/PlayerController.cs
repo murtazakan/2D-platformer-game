d048cddf0a65a43c288eebb017c66589767bf844
murkeh <kanorwalamurtaza217@gmail.com>
2020-09-25 20:30:47 +0530

    Chapter 3 - Challenge 1 - Crouch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,11 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
-        
     }
 
     private void Update()
     {
+        //walk animation
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
 
@@ -27,5 +27,35 @@ public class PlayerController : MonoBehaviour
         }
         
         transform.localScale = scale;
+
+        //crouch animation
+        BoxCollider2D crouchCollider, jumpCollider;
+        crouchCollider = GetComponent<BoxCollider2D>();
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", true);
+            //Debug.Log("Ctrl key is pressed");
+            crouchCollider.offset = new Vector3(-0.1084875f,0.5984814f);
+            crouchCollider.size = new Vector3(0.9654864f,1.302302f);
+        }
+        else
+        {
+            animator.SetBool("Crouch", false);
+            crouchCollider.offset = new Vector3(-0.01487844f, 0.9900814f);
+            crouchCollider.size = new Vector3(0.4513782f, 2.020973f);
+        }
+
+        //jump animation
+        float jump = Input.GetAxisRaw("Vertical");
+
+        if (jump > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else {
+            animator.SetBool("Jump", false);
+        }
+
     }
 }
